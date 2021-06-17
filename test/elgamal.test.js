868acb344a1db857a307aea89c3bf1d2dd098db8
SherLzp <lzp.sher@gmail.com>
2021-06-17 10:37:38 +0800

    [Sher] optimize ElGamal dec

--- test/elgamal.test.js
+++ test/elgamal.test.js
@@ -4,10 +4,14 @@ const { genKeyPair, elgamalEnc, elgamalDec, randomValue } = require("../dist");
 test("ElGamal Encryption & Decryption", function (t) {
   t.plan(1);
   const { sk, pk } = genKeyPair();
-  const Max = BigInt(10000);
-  const b = BigInt(200);
+  const Max = BigInt(1000000);
+  const b = BigInt(100000);
   const r = randomValue();
+  console.time('enc');
   const enc = elgamalEnc(b, r, pk);
+  console.timeEnd('enc');
+  console.time('dec');
   const bprime = elgamalDec(enc, sk, Max);
+  console.timeEnd('dec');
   t.equal(bprime, b, 'ElGamal works correctly');
 });
