70744e5b179eaf7d93bae789e73100b05d719413
SherLzp <lzp.sher@gmail.com>
2021-06-19 11:27:57 +0800

    [Sher] add tests for elgamalEncAdd

--- test/elgamal.test.js
+++ test/elgamal.test.js
@@ -1,17 +1,21 @@
 const test = require("tape");
-const { genKeyPair, elgamalEnc, elgamalDec, randomValue } = require("../dist");
+const {genKeyPair, elgamalEnc, elgamalDec, randomValue, elgamalEncAdd} = require("../dist");
 
 test("ElGamal Encryption & Decryption", function (t) {
-  t.plan(1);
-  const { sk, pk } = genKeyPair();
-  const Max = BigInt(1000000);
-  const b = BigInt(10);
-  const r = randomValue();
-  console.time('enc');
-  const enc = elgamalEnc(b, r, pk);
-  console.timeEnd('enc');
-  console.time('dec');
-  const bprime = elgamalDec(enc, sk, Max);
-  console.timeEnd('dec');
-  t.equal(bprime, b, 'ElGamal works correctly');
+    t.plan(1);
+    const {sk, pk} = genKeyPair();
+    const Max = BigInt(1000000);
+    const b = BigInt(10);
+    const bDelta = BigInt(-4);
+    const r = randomValue();
+    const r2 = randomValue();
+    console.time('enc');
+    var enc = elgamalEnc(b, r, pk);
+    const CDelta = elgamalEnc(bDelta, r2, pk);
+    enc = elgamalEncAdd(enc, CDelta);
+    console.timeEnd('enc');
+    console.time('dec');
+    const bprime = elgamalDec(enc, sk, Max);
+    console.timeEnd('dec');
+    t.equal(bprime, BigInt(6), 'ElGamal works correctly');
 });
